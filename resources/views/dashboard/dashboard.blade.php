b37e911207c3c8a5768f82a5c3612fb2f28f013e
Jithin Pillakkat <jithinpillakkat@gmail.com>
2026-07-31 11:36:57 +0530

    console error

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -396,7 +396,7 @@
                     }
                 },
                 error: function (xhr) {
-                    console.erro(xhr.responseJSON?.message ?? "Something went wrong.");
+                    console.error(xhr.responseJSON?.message ?? "Something went wrong.");
                 },
                 complete: function () {
 
